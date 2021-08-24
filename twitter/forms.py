9e6094b9f32156d579923a7666ce0486a706cbe1
kuniyoshi koichiro <kuniyoshi.koichiro.727@gmail.com>
2021-08-24 14:20:59 +0900

    'レビュー結果反映'

--- twitter/forms.py
+++ twitter/forms.py
@@ -1,16 +1,1 @@
 from django import forms
-from django.contrib.auth.models import User
-
-#ユーザ登録画面のフォーム
-class UserForm(forms.ModelForm):
-    #パスワード入力：非表示対応
-    password = forms.CharField(widget=forms.PasswordInput(),label="パスワード")
-
-    class Meta():
-        #Userとマッピング
-        model = User
-        #フィールド指定
-        fields = ('username','password',)
-        #フィールド名指定
-        labels = {'username':"ユーザーID",}
-
--- twitter/views.py
+++ twitter/views.py
@@ -1,43 +1,9 @@
-
-from django.shortcuts import render
-from django.views.generic import TemplateView #テンプレートタグ
-from .forms import UserForm
-from django.contrib.auth import authenticate
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.urls.base import reverse_lazy
 
 # Create your views here.
-class  AccountRegistration(TemplateView):
-    def __init__(self):
-        self.params = {
-        "AccountCreate":False,
-        "user_form": UserForm(),
-        }
-
-    #Get処理
-    def get(self,request):
-        self.params["user_form"] = UserForm()
-        self.params["AccountCreate"] = False
-        return render(request,"twitter/register.html",context=self.params)
-    
-    #Post処理
-    def post(self,request):
-        self.params["user_form"] = UserForm(data=request.POST)
-
-        #フォーム入力の有効検証
-        if self.params["user_form"].is_valid():
-            # アカウント情報をDB保存
-            user = self.params["user_form"].save(commit=False)
-            # パスワードをハッシュ化
-            user.set_password(user.password)
-            # ハッシュ化パスワード更新
-            user.save()
-            # アカウント作成情報更新
-            self.params["AccountCreate"] = True
-
-        else:
-            # フォームが有効でない場合
-            print(self.params["user_form"].errors)
-
-        return render(request,"twitter/register.html",context=self.params)
+class  AccountRegistration(CreateView):
+    form_class = UserCreationForm
+    template_name = 'twitter/register.html'
+    success_url = reverse_lazy('registeration')
