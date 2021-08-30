3405292c36bec6c141be16e2d424f5ce559a3584
kuniyoshi koichiro <kuniyoshi.koichiro.727@gmail.com>
2021-08-30 19:26:29 +0900

    ログイン機能の追加

--- twitter/urls.py
+++ twitter/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.AccountRegistration.as_view(), name='registeration'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('home/',views.HomeView.as_view(), name="home"),
 ]
